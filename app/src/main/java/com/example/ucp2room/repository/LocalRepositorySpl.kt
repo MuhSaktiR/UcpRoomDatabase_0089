a5ce956990e498fdc767a41d406e7699cfc4e101
MuhSaktiR <seken876@gmail.com>
2024-12-18 17:37:02 +0700

    Menambahkan LocalRepositoryBrg untuk Create dan Read Supplier

---
+++ app/src/main/java/com/example/ucp2room/repository/LocalRepositorySpl.kt
@@ -0,0 +1,21 @@
+package com.example.ucp2room.repository
+
+import com.example.ucp2room.data.dao.SupplierDao
+import com.example.ucp2room.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySpl (
+    private val supplierDao: SupplierDao
+) : RepositorySpl {
+    override suspend fun insertSpl(supplier: Supplier) {
+        supplierDao.insertSupplier(supplier)
+    }
+
+    override fun getAllSpl(): Flow<List<Supplier>> {
+        return supplierDao.getAllSupplier()
+    }
+
+    override fun getSpl(id: String): Flow<Supplier> {
+        return supplierDao.getSupplier(id)
+    }
+}
