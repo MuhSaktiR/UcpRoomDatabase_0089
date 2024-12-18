26f10eacfb54c4597602bd29ab5365f3e920c4d7
MuhSaktiR <seken876@gmail.com>
2024-12-18 17:35:38 +0700

    Menambahkan LocalRepositoryBrg untuk CRUD Barang

---
+++ app/src/main/java/com/example/ucp2room/repository/LocalRepositoryBrg.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2room.repository
+
+import com.example.ucp2room.data.dao.BarangDao
+import com.example.ucp2room.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryBrg (
+    private val barangDao: BarangDao
+) : RepositoryBrg {
+    override suspend fun insertBrg(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+
+    override fun getAllBrg(): Flow<List<Barang>> {
+        return barangDao.getAllBarang()
+    }
+
+    override fun getBrg(id: String): Flow<Barang> {
+        return barangDao.getBarang(id)
+    }
+
+    override suspend fun deleteBrg(barang: Barang) {
+        barangDao.deleteBarang(barang)
+    }
+
+    override suspend fun updateBrg(barang: Barang) {
+        barangDao.updateBarang(barang)
+    }
+
+}
