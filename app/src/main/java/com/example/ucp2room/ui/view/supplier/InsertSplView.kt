2f1c170f111eac67b28c83a7312b19053d50f064
MuhSaktiR <seken876@gmail.com>
2024-12-24 10:21:37 +0700

    Membuat view InsertSplView untuk input atau insert form data Supplier

---
+++ app/src/main/java/com/example/ucp2room/ui/view/supplier/InsertSplView.kt
@@ -0,0 +1,186 @@
+package com.example.ucp2room.ui.view.barang
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2room.data.entity.Supplier
+import com.example.ucp2room.ui.customwidget.TopAppBar
+import com.example.ucp2room.ui.navigation.AlamatNavigasi
+import com.example.ucp2room.ui.viewmodel.supplier.FormErrorState
+import com.example.ucp2room.ui.viewmodel.supplier.SupplierEvent
+import com.example.ucp2room.ui.viewmodel.supplier.InsertSplViewModel
+import com.example.ucp2room.ui.viewmodel.supplier.SplUIState
+import com.example.ucp2room.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertSplView(
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    viewModel: InsertSplViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
+    val snackbarHostState =  remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage)  {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Supplier",
+                modifier = modifier
+            )
+        }
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+
+            InsertBodySpl(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    viewModel.saveData()
+                    if (uiState.snackBarMessage == "Data Berhasil disimpan")
+                    {
+                        onNavigate()
+                    }
+                }
+            )
+
+        }
+
+    }
+}
+
+@Composable
+fun InsertBodySpl(
+    modifier: Modifier = Modifier,
+    onValueChange: (SupplierEvent) -> Unit,
+    uiState: SplUIState,
+    onClick: () -> Unit
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormSupplier(
+            supplierEvent = uiState.supplierEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2b95bb)
+            )
+        ) {
+            Text("Simpan")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FormSupplier(
+    supplierEvent: SupplierEvent = SupplierEvent(),
+    onValueChange: (SupplierEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.nama,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(nama = it))
+            },
+            label = { Text("Nama")},
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama Supplier")}
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.kontak,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(kontak = it))
+            },
+            label = { Text("Kontak")},
+            isError = errorState.kontak != null,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            placeholder = { Text("Masukkan Nomor Kontak")}
+        )
+        Text(
+            text = errorState.kontak ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.alamat,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(alamat = it))
+            },
+            label = { Text("Alamat")},
+            isError = errorState.alamat != null,
+            placeholder = { Text("Masukkan Alamat")}
+        )
+        Text(
+            text = errorState.alamat ?: "",
+            color = Color.Red
+        )
+    }
+}
