3fa5d68b576e8400dfe9e7085267c7551f1e2c8a
MuhSaktiR <seken876@gmail.com>
2024-12-24 12:07:41 +0700

    Melengkapi objek destinasi navigasi pada alamat navigasi

--- app/src/main/java/com/example/ucp2room/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2room/ui/navigation/AlamatNavigasi.kt
@@ -8,7 +8,30 @@ object DestinasiHomeBrg : AlamatNavigasi {
     override val route = "home-brg"
 }
 
+object DestinasiHomeSpl : AlamatNavigasi {
+    override val route = "home-spl"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val ID = "id-detail"
+    val routesWithArg = "$route/{$ID}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val ID = "id-update"
+    val routesWithArg = "$route/{$ID}"
+}
+
+object DestinasiHalamanUtama : AlamatNavigasi {
+    override val route = "awalan"
+}
+
 object DestinasiInsertBrg : AlamatNavigasi {
     override val route: String = "insert-brg"
 }
 
+object DestinasiInsertSpl : AlamatNavigasi {
+    override val route: String = "insert-spl"
+}
